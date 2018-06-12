d73c14dbf0d159214b18d9f1572abc854f1a8328
LonlySimpleton <wgr850614@126.com>
2018-06-12 12:57:09 +0800

    commit test dictionary

---
+++ test/temp.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+import tkinter
+import time
+import datetime
+
+def gettime():
+          s = str(datetime.datetime.now())+'\n'
+          txt.insert(tkinter.END,s)
+
+          root.after(1000,gettime)
+
+root = tkinter.Tk()
+root.geometry('320x240')
+txt = tkinter.Text(root)
+txt.pack()
+gettime()
+root.mainloop()
+#第二次修改，将上一次注释删除
+#第三次修改
+#第四次修改`
